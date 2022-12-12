5a3f33caee472e8d8ee36cae1d34ebc6fbeff287
azh <alicezh15@gmail.com>
2022-12-12 14:01:47 -0800

    Create .startup.sh

---
+++ .startup.sh
@@ -0,0 +1,22 @@
+# Run this file to set up dotfiles and configs
+
+#!/usr/bin/env bash
+
+set -eu pipefail -x
+
+# Set up git autocomplete
+curl -o ~/.git-completion.bash  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+source ~/.git-completion.bash
+
+# Set up git prompt
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+source ~/.git-prompt.sh
+
+GIT_PS1_SHOWCOLORHINTS=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWDIRTYSTATE=true
+PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
+
+# Source all dotfiles
+source ~/.aliases
+source ~/.bindings
