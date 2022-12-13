3ef0a3a97f0b2eb9664bf8b863ee11c5717ff4a5
azh <alicezh15@gmail.com>
2022-12-13 11:44:13 -0800

    Update .startup.sh

--- .startup.sh
+++ .startup.sh
@@ -4,17 +4,11 @@ set -eu pipefail -x
 
 # Set up git autocomplete
 curl -o ~/.git-completion.bash  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-source ~/.git-completion.bash
+curl -o ~/.git-completion.zsh  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
 # Set up git prompt
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-source ~/.git-prompt.sh
-
-GIT_PS1_SHOWCOLORHINTS=true
-GIT_PS1_SHOWUNTRACKEDFILES=true
-GIT_PS1_SHOWDIRTYSTATE=true
-PROMPT_COMMAND='__git_ps1 "\u@\h:\w" "\\\$ "'
 
 # Source all dotfiles
-source ~/.aliases
-source ~/.bindings
+cat ~/.init_bash >> ~/.bashrc
+cat ~/.init_zsh >> ~/.zshrc
